5d3e2a0db6a8985a31729a03dbb39732561c559c
Robert Bitel <lonecow@gmail.com>
2012-12-13 08:04:35 -0500

    Added ability to create required directories

--- Install.sh
+++ Install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+createDirectories()
+{
+   directories="/mnt/data /mnt/external /mnt/external2"
+   for directory in $directories; do
+      if [ ! -d $directory ]; then
+         mkdir -p $directory
+      fi
+   done
+}
+
+
 if [ "`whoami`" != "root" ]; then
    echo "You do not have permissions to execute this command"
    exit 255
@@ -27,3 +38,4 @@ fi
 service smbd resart
 service transmission-daemon restart
 
+createDirectories
