95bf9c2fdd1e20b48bb2f516492693e794c8ca9e
Robert Bitel <lonecow@gmail.com>
2013-01-09 15:55:01 -0500

    Fixed permissions on Transmission users

--- TRANSMISSION/Install-Transmission.sh
+++ TRANSMISSION/Install-Transmission.sh
@@ -8,6 +8,7 @@ fi
 apt-get install transmission-daemon
 
 usermod -g transmission-users debian-transmission
+groupdel debian-transmission
 
 if [ -d /etc/transmission-daemon ]; then
    cp -v `dirname $0`/transmission/* /etc/transmission-daemon/
@@ -15,6 +16,9 @@ else
    echo "Transmission-daemon doesnt seem to exist"
    exit 255
 fi
+
+chown debian-transmission /mnt/data/Downloads
+chown debian-transmission /mnt/data/PartialDownloads
 
 service transmission-daemon restart
 
